8f7e3f4a6d11e08284b3e974bb257277a5eeaefb
Unathi Chonco <choncou@gmail.com>
2016-01-21 10:18:40 -0800

    New design

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready($(function(){
+    $("#title-intro").typed({
+        strings: ["Hello,", "My name is"],
+        typeSpeed: 10,
+        backSpeed: 1
+    });
+    $("#title-name").typed({
+        strings: ["Unathi Chonco"],
+        typeSpeed: 5,
+        startDelay: 2000
+    });
+}));
